95ece21b17a838b0cde23fd8baddcc46b80b5c06
Mark Voschezang <mark.voschezang@>
2022-07-08 12:42:03 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='templates',
+      version='0.1.0',
+      description='Various templates',
+      url='https://github.com/voschezang/data-science-templates',
+      author='Voschezang',
+      author_email='',
+      license='GNU General Public License v3.0',
+      packages=['.'],
+      install_requires=['numpy',
+                        ])
