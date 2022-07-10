20248bbbd129d8a4d5c0a50235ac19776019a2aa
Mark Voschezang <mark.voschezang@>
2022-07-10 14:46:16 +0200

    add progress bar

---
+++ src/progress_bar.py
@@ -0,0 +1,16 @@
+
+from itertools import cycle
+
+dot = cycle(r'. ')
+dots = cycle(r'.: ')
+pipe = cycle(r'|/-\\')
+pipe = cycle(r'|/-\\')
+line = cycle([
+    '[  ]',
+    '[- ]',
+    '[ -]',
+])
+
+
+def show(loop=dots):
+    print(' ' + next(loop), end='\r')
