9153885712b7168b4712d47e4f793eed2af1aa7f
Mark Voschezang <mark.voschezang@student.uva.nl>
2022-11-05 11:57:52 +0100

    update test with table data

--- test/test_html_table.py
+++ test/test_html_table.py
@@ -1,45 +1,43 @@
 from pytest import raises
 import yaml
 
-from html_table import example_yaml_data, generate, verify_table_data
+from html_table import example_yaml_data, generate
 from html_table_data import parse_json
 
 expected_html = """
 <table>
   <tr>
-    <th>First Heading</th>
-    <th>Last Heading</th>
+    <th><p><em>First</em> Heading</p>
+</th>
+    <th><p><em>Last</em> Heading</p>
+</th>
   </tr>
   <tbody>
     <tr>
-      <td rowspan="2">A value</td>
-      <td>Option B</td>
+      <td rowspan="2"><p>A value</p>
+</td>
+      <td><p>Option B</p>
+</td>
     </tr>
     <tr>
-      <td>Option C</td>
+      <td><p>Option C</p>
+</td>
     </tr>
   </tbody>
   <tbody>
     <tr>
-      <td rowspan="2">Another value</td>
-      <td>Option D</td>
+      <td rowspan="2"><p>Another value</p>
+</td>
+      <td><p>Option D</p>
+</td>
     </tr>
     <tr>
-      <td>Option E</td>
+      <td><p>Option E</p>
+</td>
     </tr>
   </tbody>
 </table>
 """
-
-
-def test_html_table_verify_data():
-    data = yaml.load(example_yaml_data, yaml.Loader)
-
-    verify_table_data(data)
-
-    del data['parameters']
-    with raises(AssertionError):
-        verify_table_data(data)
 
 
 def test_html_table_generate():
